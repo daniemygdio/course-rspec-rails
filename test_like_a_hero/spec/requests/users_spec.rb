52e9345242ca97389f64d5c0fe5af820a66705c7
Danielle Emygdio <emygdio.d@gmail.com>
2019-05-13 17:18:24 -0300

    Add IT content

--- test_like_a_hero/spec/requests/users_spec.rb
+++ test_like_a_hero/spec/requests/users_spec.rb
@@ -1,10 +1,24 @@
 require 'rails-helper'
 
 RSpec.describe "Users", type: :request do
-    describe "GET /users" do
-        it "works!" do
-            get users_path
-            expect(response).to  have_http_status(200)
-        end
-    end
+    # describe "GET /users" do
+    #     it "returns success status" do
+    #         get users_path
+    #         expect(response).to  have_http_status(200)
+    #     end
+        
+    #     it "the user's title is present"
+    # end
+
+    # describe "POST /users" do
+    #     context "when it has valid parameters" do
+    #         it "creates the user with correct attributes"
+    #     end
+
+    #     context "when it has no valid parameters" do
+    #         it "does not create user"
+    #     end
+        
+    # end
+    
 end
